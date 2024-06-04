e4713c16706e2a61fe8a0c2d17e355d5a2e21bdd
Dorian Devua <Dorian.DevUa@gmail.com>
2024-06-04 11:32:02 +0400

    add App component to the project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import { Wrapper } from './App.styled';
+
+export class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <p>Hello World!</p>
+        <p>Hello World!</p>
+      </Wrapper>
+    );
+  }
+}
