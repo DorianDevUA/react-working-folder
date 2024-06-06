c86351f4945291578b7300ccf4cf6b9a437c95d9
Dorian Devua <Dorian.DevUa@gmail.com>
2024-06-06 14:43:58 +0400

    Add toggleModal, handleKeydown and handleBackdropClick functions to work with the Modal component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,39 @@
 import { Component } from 'react';
 import { Wrapper } from './App.styled';
 import { Modal } from '../Modal';
+import { TestForm } from '../TestForm';
 
 export class App extends Component {
+  state = {
+    showModal: true,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+    console.log('toggle showModal');
+  };
+
   render() {
+    const { showModal } = this.state;
+
     return (
       <Wrapper>
-        <Modal>
-          <h2>Hello from Modal!</h2>
-        </Modal>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <h2>Hello from Modal!</h2>
+            <TestForm />
+          </Modal>
+        )}
         <h1>Hello World!</h1>
         <p>Nice try this is sparta</p>
+        <button type="button" onClick={() => {
+          console.log("Press Open Modal Btn");
+          this.toggleModal();
+        }}>
+          Open Modal
+        </button>
       </Wrapper>
     );
   }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,21 +7,35 @@ const modalRoot = document.getElementById('modal-root');
 export class Modal extends Component {
   componentDidMount = () => {
     window.addEventListener('keydown', this.handleKeydown);
+    console.log("Component <Modal> didMount");
   };
 
   componentWillUnmount = () => {
     window.removeEventListener('keydown', this.handleKeydown);
+    console.log("Component <Modal> willUnmount");
   };
 
-  handleKeydown = (evt) => {
+  handleKeydown = evt => {
+    const { onClose } = this.props;
+
     if (evt.code === 'Escape') {
       console.log('Натиснули ESC');
+
+      // Call the toggleModal function as onClose()
+      // OnClose comes as a component property from App.jsx
+      onClose();
     }
   };
 
-  handleBackdropClick = (evt) => {
+  handleBackdropClick = evt => {
+    const { onClose } = this.props;
+
     if (evt.target === evt.currentTarget) {
       console.log('Click on Backdrop!');
+
+      // Call the toggleModal function as onClose()
+      // OnClose comes as a component property from App.jsx
+      onClose();
     }
   };
 
